2a0e54f8c60b5e82546ad246bc39c252955d65b5
tfermoso <tomas.fermoso@gmail.com>
2025-02-17 19:03:49 +0100

    ejemplo de vista

--- app/controllers/ActorController.php
+++ app/controllers/ActorController.php
@@ -2,8 +2,11 @@
 require_once("./core/Controller.php");
 
 class ActorController extends Controller{
-    public function index(){
+    public function index(...$params){
         echo "hola desde index de ActorController";
+    }
+    public function new(...$params){
+        $this->view("new_actor")
     }
 
 }
